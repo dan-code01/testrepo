1faf42be7759fed186f5d20bf692c0854fc70d90
dan-code01 <235916824+dan-code01@users.noreply.github.com>
2025-12-02 08:11:25 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+""" This code will:
+Prompt for your name
+Set the name variable
+Print a message"""
+
+name = input("What is your name? ") # gets the user's name
+print("Hello, " + name)
+
+print("Hello, Daniel")
